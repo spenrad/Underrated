fcd67129a6bdc2d50d5f8a00c5cd4b3b96b3fc65
Spencer Christy <spenrad@gmail.com>
2021-02-05 11:58:18 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controller/books_controller.js');
+
+app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
