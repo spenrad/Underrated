ceb92dc2f071c3475e472832680b88c901cca3e0
Spencer Christy <spenrad@gmail.com>
2021-02-06 19:21:51 -0800

    merging for testing in api branch

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,9 @@
 $(document).ready(function () { 
 
-var button = $(".submit");
+var button = $(".submitSearch");
 
 button.on("click", function () { 
+    $(".searchResults").empty()
     $.ajax({
         url : `http://www.omdbapi.com/?apikey=b9e5adb0&s=${$("#searchBar").val()}`,
         method: "GET"
@@ -24,14 +25,14 @@ button.on("click", function () {
                 var img = $("<img>");
                 var btn1= $("<button>");
                 var btn2= $("<button>");
-                h2.text(response.Title)
-                h3.text(response.Year);
+                h2.text(response.Title + " (" + response.Year + ") ")
+                h3.text(response.Genre + " | " + "Rated: " + response.Rated);
                 p.text(response.Plot);
                 img.attr("src", response.Poster);
                 btn1.text("Watch List").attr("id", "watchList");
                 btn2.text("Seen it!").attr("id", "reviews");
 
-                $(".container").append(h2, h3, p, img, btn1, btn2);
+                $(".searchResults").append(h2, h3, img, p, btn1, btn2);
             
             })}
         });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,7 +4,7 @@ var express = require("express");
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('index');
+    res.render('search');
 });
 
 module.exports = router;
