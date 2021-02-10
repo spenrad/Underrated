4ad1d5f544107d40659e41736e93a1076013b5e4
Sammy Kroner <sammyk118@gmail.com>
2021-02-10 11:59:25 -0800

    watchlist html, some review handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,68 +19,75 @@ router.get('/signup', function (req, res) {
 
 router.get('/user/:profile', function (req, res) {
     // const hbsObject = {movies: []}
-
+    var hbsObject = {};
     console.log("==========================================")
     var profile = req.params.profile;
     console.log(profile);
-
-
-
 
     db.User.findOne({
         where: {
             username: profile
         }
     }).then(function (userInf) {
-        
+
         console.log("user info: ", userInf);
         console.log("========")
         console.log(userInf.dataValues);
-        const hbsObjectName = {
-            userName: userInf.dataValues.username
-        };
-        res.render("profile", hbsObjectName)
+
+        hbsObject.userName = userInf.dataValues.username;
         // res.render("profile", hbsObjectName)
-    
+
+
         // console.log(hbsObject);
-        // res.render("profile", hbsObject);
+
         db.UserMovie.findAll({
             where: {
                 userID: userInf.id
             }
         }).then(function (reviewInf) {
-     
+            reviewArr = [];
             const hbsObjectUserMovie = {
                 userMovie: reviewInf
             }
+            for (i = 0; i < reviewInf.length; i++){
+                reviewArr.push(reviewInf[i].dataValues)
+                console.log("TEST LOG ==========", reviewInf[i].dataValues)
+            }
+            hbsObject.reviews = reviewArr;
             console.log("review info=========================== ", "\n", reviewInf)
+            var promiseArr = [];
 
             for (i = 0; i < reviewInf.length; i++) {
-                db.Movie.findOne({
-                    where: {
-                        ID: reviewInf[i].dataValues.movieID
-                    }
-                }).then(function (results) {
-                    console.log("movie database ========================", "\n", results.dataValues)
-                    axios.get("http://www.omdbapi.com/?apikey=b9e5adb0&i=" + results.dataValues.imdbID)
-                        .then(function (response) {
-                            // console.log("axios query===============================", "\n", response.data);
-                     
-                            const hbsObjectAPI = {
-                                omdb: response.data
-                            }
-                            console.log("OBJECTAPI===========================", "\n", hbsObjectAPI);
-                        })
+                promiseArr.push(
+                    db.Movie.findOne({
+                        where: {
+                            ID: reviewInf[i].dataValues.movieID
+                        }
+                    }).then(function (results) {
+                        // console.log("movie database ========================", "\n", results.dataValues)
 
-                })
+                        return axios.get("http://www.omdbapi.com/?apikey=b9e5adb0&i=" + results.dataValues.imdbID)
+                            .then(function (response) {
+                                // console.log("axios query===============================", "\n", response.data);
+                           
+                                return response.data;
 
 
+                            });
 
+                    })
+                );
             }
+            Promise.all(promiseArr).then(function (responses) {
+
+                hbsObject.movies = responses;
+                console.log("FINAL OBJECT==========", hbsObject);
+
+                res.render("profile", hbsObject);
+            })
             // console.log("MOVIE INFO =========================")
-            console.log("OBJECTname===========================", "\n", hbsObjectName);
-            console.log("OBJECTmovies===========================", "\n", hbsObjectUserMovie);
-         
+       
+
 
         })
 
@@ -90,19 +97,19 @@ router.get('/user/:profile', function (req, res) {
 
     })
 })
-            // }
-                // Promise.all(promises)
-                // .then(function () {
-                //     var hbsObject = {
-                //         movies: moviesArr
-                //     }
-                //     console.log("FINAL OBJECT =====================")
-                //     console.log(hbsObject)
-                //     res.render("profile", hbsObject)
-                // })
+// }
+// Promise.all(promises)
+// .then(function () {
+//     var hbsObject = {
+//         movies: moviesArr
+//     }
+//     console.log("FINAL OBJECT =====================")
+//     console.log(hbsObject)
+//     res.render("profile", hbsObject)
+// })
 
 
-            
+
 
 
 
